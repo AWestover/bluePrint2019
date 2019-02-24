427996cf24f29dafbc452fb78685e9c0a71dbd9d
Alek Westover <alek.westover@gmail.com>
2019-02-24 11:20:16 -0500

    test

--- test/test.py
+++ test/test.py
@@ -1,8 +1,20 @@
 
+import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 fn = "img.jpg"
-img = cv2.imread(fn, cv2.IMREAD_GRAYSCALE)
+img = cv2.imread(fn)
+# edges = cv2.Canny(img,100,200)
 
-plt.imshow(img)
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2)
+
+# LOWER_GREY = np.arry([211,211,211])
+# UPPER_GREY = np.array([0,0,0])
+
+# colorGreyScores = np.logical_and(np.sum(img, axis=2) < 600, ((img[:,:,0]-img[:,:,1])**2+(img[:,:,1]-img[:,:,2])**2+(img[:,:,0]-img[:,:,2])**2) < 100)
+colorGreyScores = ((img[:,:,0]-img[:,:,1])**2+(img[:,:,1]-img[:,:,2])**2+(img[:,:,0]-img[:,:,2])**2) < 10
+
+plt.imshow(img,cmap='gray')
+plt.show()
+plt.imshow(colorGreyScores,cmap='gray')
 plt.show()
